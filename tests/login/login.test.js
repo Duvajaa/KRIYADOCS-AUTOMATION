187a4a95218bf372cb30a41ab48c0b7a92dd3e9e
anwin <anwinsjohn2001@gmail.com>
2024-03-27 15:03:11 +0530

    login test cases updated

---
+++ tests/login/login.test.js
@@ -0,0 +1,60 @@
+
+const { test, expect } = require('@playwright/test');
+var dotenv = require('dotenv');
+dotenv.config();
+
+
+test.describe('Kriya_login', async() => {
+    
+    //to check valid credentials
+    test('KD-TC-5370: User Should be able to login with valid credentials',async({ browser }) => {
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto(process.env.siteName);
+        await page.locator('#username').fill(process.env.kusername);
+        await page.locator('#password').fill(process.env.password);
+        await page.locator('.input-field.col.s12.login.center.loginButton').click()
+        await page.waitForTimeout(2000);
+        if (await page.locator('.col.s6.confirmationPanel').isVisible())
+        {
+            await page.locator('.btn.waves-effect.waves-light.confirm').click();
+        }
+        await page.waitForSelector('#customerSelectionDiv .customerTitle .customerTitleDiv');
+        const dboard = page.locator('#customerSelectionDiv .customerTitle .customerTitleDiv');
+        await expect(dboard).toBeVisible();
+    })
+   
+    //to check invalid credentials
+    test('KD-TC-5371: User Should be able to login with invalid credentials and show error',async({ page }) => {
+        await page.goto(process.env.siteName);
+        await page.locator('#username').fill(process.env.kusername);
+        await page.locator('#password').fill('HappyAuthors');
+        await page.locator('.input-field.col.s12.login.center.loginButton').click();
+        const ertext = page.locator('.input-field.col.s12.login.center.loginButton .error-text');
+        await expect(ertext).toHaveText(' Invalid credentials ');
+    })
+     
+    //reset link is sent
+    test('KD-TC-5372: forget password should send a reset link in email to user with valid email',async({ page }) => {
+        await page.goto(process.env.siteName);
+        if(await page.locator('.row.login-view.forgetpassword.forgetPass').click())
+        {
+            await page.locator('col s6 login-col-2').isVisible()
+            await page.locator('#username').fill(process.env.kusername);
+            const reset = page.locator('#forgetMessage');
+            await expect(reset).toHaveText(' Link sent to your respective email id ');
+        }
+       
+    })
+    
+    //empty username and password
+    test('KD-TC-5373: User Should not be allowed to login with empty username and password ',async({ page }) => {
+        await page.goto(process.env.siteName);
+        await page.locator('.input-field.col.s12.login.center.loginButton').click();
+        const invuser = page.locator('#username.validate.formfields.invalid');
+        const invpass = page.locator('#password.validate.formfields.invalid');
+        await expect(invuser,invpass).toBeVisible();
+    })
+    
+})
+

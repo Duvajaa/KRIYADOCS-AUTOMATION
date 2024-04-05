dea552b48d8d1c337f9f297a0334c71b1b267f00
anwin <anwinsjohn2001@gmail.com>
2024-04-05 18:43:44 +0530

    report updt

--- tests/reports/reports.test.js
+++ tests/reports/reports.test.js
@@ -12,8 +12,8 @@ test.describe('reports',async() => {
         await page.waitForSelector('.card .cusTitle[data-customer-name="bmj"]');
         await page.click('.card .cusTitle[data-customer-name="bmj"]');
         //await page.waitForSelector('.nav-item .nav-link.reports.nav_list1.active')
-        await page.waitForLoadState();
-        await page.getByText('Reports').click();
+       // await page.waitForLoadState();
+        await page.getByText('Reports').click();// add css selectors
         await page.waitForSelector('.card-widget.row[data-report="All Article summary"]');
         await page.click('.card-widget.row[data-report="All Article summary"]');
         const report = page.locator('#dailyReportDiv');
@@ -23,6 +23,7 @@ test.describe('reports',async() => {
         const Tdate = page.locator('#toDate');
         await expect(Tdate).toBeVisible();
 
+        //add locators for both from date and to date
         await page.click('.datepicker--content .datepicker--cell.datepicker--cell-day.-weekend-.-selected-[data-date="6"]');
         await page.click('.datepicker--content .datepicker--cell.datepicker--cell-day.-weekend-.-selected-[data-date="7"]');
         await expect(report).toBeEmpty();
@@ -41,12 +42,12 @@ test.describe('reports',async() => {
 
         // Wait for the download process to complete and save the downloaded file somewhere.
         await download1.saveAs("./tests/reports" + download1.suggestedFilename());
-        
+        //insert dropdown method
         // Start waiting for download before clicking. Note no await.
         const downloadPromise2 = page.waitForEvent('download');
         await page.locator('.dropdown-item[onclick="eventHandler.components.actionitems.exportCSV(this,event)"]').click();
         const download2 = await downloadPromise2;
-        
+        //insert wait
         // Wait for the download process to complete and save the downloaded file somewhere.
         await download2.saveAs("./tests/reports" + download2.suggestedFilename());
     })
